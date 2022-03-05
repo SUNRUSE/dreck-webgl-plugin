79b1adc402c3c01b3d9f248602cb3fac856d7235
jameswilddev <jameswilddev@outlook.com>
2022-03-05 10:59:16 +0000

    Add shader type keywords.

--- src/index.ts
+++ src/index.ts
@@ -38,3 +38,4 @@ export { Vec2 } from "./Vec2";
 export { Vec3 } from "./Vec3";
 export { Vec4 } from "./Vec4";
 export { VertexBuffer } from "./VertexBuffer";
+export { shaderTypeKeyword } from "./shaderTypeKeyword";
