0f1cc16dc1fca36181a7865c9964d42274b5f4ac
jameswilddev <jameswilddev@outlook.com>
2022-03-06 16:00:29 +0000

    Organize into areas.

---
+++ src/webgl/binaryTypeType/unit.ts
@@ -0,0 +1,23 @@
+import { binaryTypeType, Constants } from "../..";
+
+describe(`binaryTypeType`, () => {
+  it(`byte`, () => {
+    expect(binaryTypeType[Constants.Byte]).toEqual(`integer`);
+  });
+
+  it(`unsigned byte`, () => {
+    expect(binaryTypeType[Constants.UnsignedByte]).toEqual(`integer`);
+  });
+
+  it(`short`, () => {
+    expect(binaryTypeType[Constants.Short]).toEqual(`integer`);
+  });
+
+  it(`unsigned short`, () => {
+    expect(binaryTypeType[Constants.UnsignedShort]).toEqual(`integer`);
+  });
+
+  it(`float`, () => {
+    expect(binaryTypeType[Constants.Float]).toEqual(`float`);
+  });
+});
