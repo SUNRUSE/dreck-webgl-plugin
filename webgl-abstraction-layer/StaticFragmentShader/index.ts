a8c57cea1826943debcdcd59ecb0297bbd9e24a6
jameswilddev <jameswilddev@outlook.com>
2022-03-11 20:24:25 +0000

    Add missing comments.

--- webgl-abstraction-layer/StaticFragmentShader/index.ts
+++ webgl-abstraction-layer/StaticFragmentShader/index.ts
@@ -15,9 +15,11 @@ export class StaticFragmentShader<
   /**
    * Creates a new WebGL fragment shader, the source of which is pre-defined at construction.
    * @param context The context under which the shader will be created.
+   * @param headerLines The lines of the shader preceding the declarations and body.
    * @param attributeDefinitionSet The attributes fed into the fragment shader.
    * @param uniformDefinitionSet The uniforms fed into the fragment shader.
    * @param varyingDefinitionSet The varyings output by the fragment shader.
+   * @param bodyLines The lines of the shader following the header and declarations.
    */
   constructor(
     context: ContextInterface<
--- webgl-abstraction-layer/StaticVertexShader/index.ts
+++ webgl-abstraction-layer/StaticVertexShader/index.ts
@@ -22,9 +22,11 @@ export class StaticVertexShader<
   /**
    * Creates a new WebGL vertex shader, the source of which is pre-defined at construction.
    * @param context The context under which the shader will be created.
+   * @param headerLines The lines of the shader preceding the declarations and body.
    * @param attributeDefinitionSet The attributes fed into the vertex shader.
    * @param uniformDefinitionSet The uniforms fed into the vertex shader.
    * @param varyingDefinitionSet The varyings output by the vertex shader.
+   * @param bodyLines The lines of the shader following the header and declarations.
    */
   constructor(
     context: ContextInterface<
